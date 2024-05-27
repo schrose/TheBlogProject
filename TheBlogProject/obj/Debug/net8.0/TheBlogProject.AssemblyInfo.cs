31ad91cd96224dd6391f67cf17217e817ae30cc4
Sean Schroder <seanschroder@icloud.com>
2024-05-26 19:39:54 -0500

    Feature series - Comments Part 2 - Listing DONE

--- TheBlogProject/obj/Debug/net8.0/TheBlogProject.AssemblyInfo.cs
+++ TheBlogProject/obj/Debug/net8.0/TheBlogProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheBlogProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519f33c760759cfcbdf8d38196aad5b1e32af83e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678839b80e0e15fca1f2036df179a959cb1cb815")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheBlogProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheBlogProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
